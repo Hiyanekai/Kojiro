008e7a94a7e8cf8f86acf03578472ec799ffa33b
hiyane <kai3883h@outlook.jp>
2024-06-12 12:42:26 +0900

    first commit

---
+++ Kojiro/src/main/java/com/example/Kojiro/KojiroApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kojiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KojiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KojiroApplication.class, args);
+	}
+
+}
