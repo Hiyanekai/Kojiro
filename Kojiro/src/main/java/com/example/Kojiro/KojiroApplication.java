1f1e6a3f4dac48a1f844cfeccd17616e3fbcf882
ihara <iharayuuta.2024.kensyu@gmail.com>
2024-06-19 17:12:32 +0900

    ihara_0619①

--- Kojiro/src/main/java/com/example/Kojiro/KojiroApplication.java
+++ Kojiro/src/main/java/com/example/Kojiro/KojiroApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
-KojiroApplication {
+public class KojiroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KojiroApplication.class, args);
--- Kojiro/src/main/java/com/example/Kojiro/controller/TermController.java
+++ Kojiro/src/main/java/com/example/Kojiro/controller/TermController.java
@@ -4,10 +4,12 @@ import com.example.Kojiro.service.QManagementService;
 import com.example.Kojiro.service.TermService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class TermController {
 
     @Autowired
@@ -15,17 +17,17 @@ public class TermController {
     @Autowired
     private HttpSession session;
 
-    @GetMapping("/quiz-management")
+    @GetMapping("/term")
     public String menu(@RequestParam(name="keyword" ,defaultValue = "") String keyword, Model model) {
 //        if (session.getAttribute("user")==null){//ユーザーのセッション判定
 //            return "redirect:/login-test";
 //        }
         if (keyword.isEmpty()){//検索欄にキーワードなしは全部出す
-            model.addAttribute("management", termService.findAll());
+            model.addAttribute("termlist", termService.findAll());
         }else {//検索欄にキーワード入れると問題文から抽出
             System.out.println(termService.findByTerm(keyword));
-            model.addAttribute("management", termService.findByTerm(keyword));
+            model.addAttribute("termlist", termService.findByTerm(keyword));
         }
-        return "quiz-management";
+        return "term";
     }
 }
