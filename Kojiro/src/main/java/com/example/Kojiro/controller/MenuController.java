544a80c43c115443172bf71ee887b3fde6e49d87
asato <24asato_takahiro@axiz.co.jp>
2024-06-21 12:03:42 +0900

    asato_0620 問題編集完成

--- Kojiro/src/main/java/com/example/Kojiro/controller/MenuController.java
+++ Kojiro/src/main/java/com/example/Kojiro/controller/MenuController.java
@@ -1,9 +1,11 @@
 package com.example.Kojiro.controller;
 
+import com.example.Kojiro.entity.Users;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -14,8 +16,11 @@ public class MenuController {
     private HttpSession session;
 
     @GetMapping("/menu")
-    public String menu(){
+    public String menu(Model model){
         if(request.getSession(false)==null) return "redirect:/index";
+        var user = (Users)session.getAttribute("users");
+        System.out.println(user.role());
+        model.addAttribute("user", user);
         return "menu";
     }
 }
